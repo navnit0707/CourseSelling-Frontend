2ebbecaa8512d8944a597bb220b57160973d3cbf
navnit0707 <knavnit81@gmail.com>
2023-07-25 21:45:13 +0530

    frontend added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Signin from "./components/Signin.jsx";
+import Signup from "./components/Signup.jsx";
+import Appbar from "./components/Appbar.jsx";
+import AddCourse from "./components/AddCourse.jsx";
+import Courses from "./components/Courses";
+import Course from "./components/Course";
+import { Landing } from "./components/Landing.jsx";
+import { userState } from "./store/atoms/user.js";
+import { RecoilRoot, useSetRecoilState } from "recoil";
+import axios from "axios";
+import { BASE_URL } from "./config.js";
+import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <RecoilRoot>
+      <div
+        style={{ width: "100vw", height: "100vh", backgroundColor: "#eeeeee" }}>
+        <Router>
+          <Appbar />
+          <InitUser />
+          <Routes>
+            <Route path={"/addcourse"} element={<AddCourse />} />
+            <Route path={"/course/:courseId"} element={<Course />} />
+            <Route path={"/courses"} element={<Courses />} />
+            <Route path={"/signin"} element={<Signin />} />
+            <Route path={"/signup"} element={<Signup />} />
+            <Route path={"/"} element={<Landing />} />
+          </Routes>
+        </Router>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </RecoilRoot>
+  );
 }
 
-export default App
+function InitUser() {
+  const setUser = useSetRecoilState(userState);
+  const init = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/admin/me`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      });
+
+      if (response.data.username) {
+        setUser({
+          isLoading: false,
+          userEmail: response.data.username,
+        });
+      } else {
+        setUser({
+          isLoading: false,
+          userEmail: null,
+        });
+      }
+    } catch (e) {
+      setUser({
+        isLoading: false,
+        userEmail: null,
+      });
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return <></>;
+}
+
+export default App;
